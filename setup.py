0f981b15f1395e49b5242501eff193e99b0c5821
Rémi GASCOU (Podalirius) <podalirius@protonmail.com>
2026-02-01 21:51:03 +0100

    Bump version from 0.1.0 to 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pywhisker",
-    version="0.1.0",
+    version="1.0.0",
     author="Charlie Bromberg & Podalirius",
     author_email="",
     description="Python (re)setter for property msDS-KeyCredentialLink for Shadow Credentials attacks",
